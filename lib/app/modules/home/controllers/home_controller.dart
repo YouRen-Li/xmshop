0a0db43b4cc26c2cc9033adfac1c2e41cabb6cc1
View <lijunpg@foxmail.com>
2025-08-04 23:14:11 +0800

    feat:调取了分类接口，渲染了页面。

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,24 +1,36 @@
 import 'dart:developer';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:xmshop/app/models/category_model.dart';
 import 'package:xmshop/app/models/focus_model.dart';
 
 class HomeController extends GetxController {
   final ScrollController scrollController = ScrollController();
+  //接口列表
+  RxList<FocusItemModel> swiperList = <FocusItemModel>[].obs;
+  RxList<CategoryItemModel> categoryList = <CategoryItemModel>[].obs;
 
   //浮动导航的样式
   final flag = false.obs;
-  //轮播图列表
-  RxList<FocusItemModel> swiperList = <FocusItemModel>[].obs;
 
   @override
   void onInit() {
     super.onInit();
     scrollControllerListener();
-    //请求轮播图接口
+    //请求接口
     getFocusData();
+    getCategoryData();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
   }
 
   // 滚动到一定距离以后搜索框样式改变
@@ -33,16 +45,6 @@ class HomeController extends GetxController {
     });
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
   // 获取轮播图
   getFocusData() async {
     var response = await Dio().get('https://miapp.itying.com/api/focus');
@@ -51,6 +53,17 @@ class HomeController extends GetxController {
     for (int i = 0; i < swiperList.length; i++) {
       swiperList[i].pic = "https://miapp.itying.com/${swiperList[i].pic}"
           .replaceAll("\\", '/');
+    }
+  }
+
+  // 获取分类
+  getCategoryData() async {
+    var response = await Dio().get('https://miapp.itying.com/api/bestCate');
+    var category = CategoryModel.fromJson(response.data);
+    categoryList.value = category.result!;
+    for (int i = 0; i < categoryList.length; i++) {
+      categoryList[i].pic = "https://miapp.itying.com/${categoryList[i].pic}"
+          .replaceAll("\\", '/'); //把\转换为/，需要转义所以是\\
     }
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -79,6 +79,118 @@ class HomeView extends GetView<HomeController> {
     );
   }
 
+  //轮播图
+  Widget _focus() {
+    return SizedBox(
+      width: Screenadapter.width(1080),
+      height: Screenadapter.height(682),
+      child: Obx(
+        () => Swiper(
+          itemBuilder: (context, index) {
+            return Image.network(
+              "${controller.swiperList[index].pic}",
+              fit: BoxFit.fill,
+            );
+          },
+          itemCount: controller.swiperList.length,
+          pagination: const SwiperPagination(builder: SwiperPagination.rect),
+          autoplay: true,
+          loop: true,
+        ),
+      ),
+    );
+  }
+
+  //banner 下划线代表私有的
+  Widget _Banner() {
+    return SizedBox(
+      width: Screenadapter.width(1080),
+      height: Screenadapter.height(92),
+      child: Image.asset("assets/images/xiaomiBanner.png", fit: BoxFit.cover),
+    );
+  }
+
+  //顶部滑动分类
+  Widget _categoty() {
+    return SizedBox(
+      width: Screenadapter.width(1080),
+      height: Screenadapter.height(470),
+      child: Obx(
+        () => Swiper(
+          itemCount: controller.categoryList.length ~/ 10, //取整
+          itemBuilder: (context, index) {
+            return GridView.builder(
+              itemCount: 10, //一页一共多少个元素
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5, //一行5个元素
+                crossAxisSpacing: Screenadapter.width(20), //左右间距
+                mainAxisSpacing: Screenadapter.height(20), //上下间距
+              ),
+              itemBuilder: (context, i) {
+                return Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: Screenadapter.height(140),
+                      height: Screenadapter.height(140),
+                      child: Image.network(
+                        '${controller.categoryList[index * 10 + i].pic}',
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsGeometry.fromLTRB(
+                        0,
+                        Screenadapter.height(5),
+                        0,
+                        0,
+                      ),
+                      // 这里的分页采取了一个简单的算法
+                      // pageview第一个页面是索引是0，0*10,就是索引乘以每页展示的数据
+                      // 0*10=0+i,i是itemBuild展示的数据，展示10个。所以第一页就展示10个数据 0-9
+                      // 第二个页面，索引是1，1*10+i。这个时候数据就从10开始，展示10个数据。10-19
+                      // i是10，数据是不变，每页展示的10条数据不变，变化的只有从第多少条数据开始加载
+                      child: Text(
+                        '${controller.categoryList[index * 10 + i].title}',
+                        style: TextStyle(fontSize: Screenadapter.fontsize(34)),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          //自定义轮播图圆点
+          pagination: SwiperPagination(
+            margin: EdgeInsets.all(0.0),
+            builder: SwiperCustomPagination(
+              builder: (BuildContext context, SwiperPluginConfig config) {
+                return ConstrainedBox(
+                  constraints: BoxConstraints.expand(
+                    height: Screenadapter.height(20),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: const RectSwiperPaginationBuilder(
+                            color: Colors.black12,
+                            activeColor: Colors.black54,
+                          ).build(context, config),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   // 内容区域
   Widget _homePage() {
     return Positioned(
@@ -88,28 +200,7 @@ class HomeView extends GetView<HomeController> {
       bottom: 0,
       child: ListView(
         controller: controller.scrollController,
-        children: [
-          SizedBox(
-            width: Screenadapter.width(1080),
-            height: Screenadapter.height(682),
-            child: Obx(
-              () => Swiper(
-                itemBuilder: (context, index) {
-                  return Image.network(
-                    "${controller.swiperList[index].pic}",
-                    fit: BoxFit.fill,
-                  );
-                },
-                itemCount: controller.swiperList.length,
-                pagination: const SwiperPagination(
-                  builder: SwiperPagination.rect,
-                ),
-                autoplay: true,
-                loop: true,
-              ),
-            ),
-          ),
-        ],
+        children: [_focus(), _Banner(), _categoty()],
       ),
     );
   }
